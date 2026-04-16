6dc59162b82caa4ca58787944e21075f23a4d631
bruno-calasans <brunogstsho@gmail.com>
2026-04-16 14:03:46 -0300

    create: movimentationInfoMsg component

--- app/movimentations/[movimentation_id]/page.tsx
+++ app/movimentations/[movimentation_id]/page.tsx
@@ -4,11 +4,9 @@ import BackButton from "@/components/custom/buttons/BackButton";
 import Loader from "@/components/custom/Loader";
 import PageTitle from "@/components/custom/PageTitle";
 import MovimentationTabs from "@/components/movimentation/tabs/MovimentationTab";
-import MovimentationStatusBadge from "@/components/custom/badges/MovimentationStatusBadge";
 import getOneMovimentation from "@/hooks/movimentation/useGetOneMovimentation";
 import { useParams } from "next/navigation";
-import Link from "next/link";
-import { InfoIcon } from "lucide-react";
+import MovimentationInfoMsg from "@/components/movimentation/MovimentationInfoMsg";
 
 export default function MovimentationIdPage() {
   const params = useParams<{ movimentation_id: string }>();
@@ -31,27 +29,7 @@ export default function MovimentationIdPage() {
         <PageTitle>Informações da Movimentação</PageTitle>
         <BackButton to="/movimentations" label="Voltar à página de Movimentações" />
       </div>
-      <div className="flex flex-col gap-1 mb-4">
-        <p>
-          <strong>ID:</strong> #{movimentation.id}
-        </p>
-        <p className="flex gap-1">
-          <strong>Produto:</strong>{" "}
-          <Link
-            className="flex gap-1 justify-center items-center hover:underline"
-            href={`/products/${movimentation.product.id}`}
-          >
-            {movimentation.product.name}
-            <InfoIcon className="text-indigo-500" size={18} />
-          </Link>
-        </p>
-        <p>
-          <strong>Quantidade:</strong> {movimentation.amount}
-        </p>
-        <p className="flex gap-1 items-start text-center">
-          <strong>Status:</strong> <MovimentationStatusBadge movimentation={movimentation} />
-        </p>
-      </div>
+      <MovimentationInfoMsg movimentation={movimentation} />
       <MovimentationTabs movimentation={movimentation} />
     </section>
   );

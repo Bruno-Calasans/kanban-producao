79e902ad7a7065aa1e3ee384f849249d73245be5
bruno-calasans <brunogstsho@gmail.com>
2026-04-20 14:20:09 -0300

    fix: block actions if the movimentation is cancelled

--- components/process-execution/ProcessExecutionActions.tsx
+++ components/process-execution/ProcessExecutionActions.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { ProcessState } from "@/types/database.type";
 import CreateProcessExecutionDialog from "./dialogs/CreateProcessExecutionDialog";
 import CreateReprocessExecutionDialog from "./dialogs/CreateReprocessExecutionDialog";
@@ -11,11 +10,13 @@ type ProcessExecutionActionsProps = {
 
 export default function ProcessExecutionActions({ processState }: ProcessExecutionActionsProps) {
   const { previousProcess, nextProcess, movimentation } = processState;
-  const isLastProcess = nextProcess == null
+  const isLastProcess = nextProcess == null;
 
   if (processState.avaliableAmount == 0) return null;
 
-  if ((movimentation.status === "CANCELLED" || movimentation.status == "COMPLETED") && !isLastProcess) {
+  if (movimentation.status == "CANCELLED") return null;
+
+  if (movimentation.status == "COMPLETED" && !isLastProcess) {
     return null;
   }
 
@@ -23,7 +24,6 @@ export default function ProcessExecutionActions({ processState }: ProcessExecuti
     <div className="flex gap-1">
       {nextProcess && <CreateProcessExecutionDialog processState={processState} />}
       {previousProcess && <CreateReprocessExecutionDialog processState={processState} />}
-      {/* <Button size="xs">Ajustar</Button> */}
     </div>
   );
 }
